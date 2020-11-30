9944ae614dc1b492a727bf966cb71f8dbff31dd1
Rick Meels <rick.meels@gmail.com>
2020-11-30 12:05:32 +0100

    Added base project

---
+++ client/src/main/java/client/SnakeClient.java
@@ -0,0 +1,10 @@
+package client;
+
+import javax.swing.*;
+
+public class SnakeClient extends JFrame {
+    public static void main(String[] args) {
+        SnakeClient frame = new SnakeClient();
+        frame.setVisible(true);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module S3.SE.SNAKE {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+
+    opens sample;
+}
